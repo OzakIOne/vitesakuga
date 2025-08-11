31387510e826a344f076be2fbb01ac06d518d89f
ozakione <29860391+OzakIOne@users.noreply.github.com>
2025-08-11 20:54:54 +0200

    update deps

--- src/router.tsx
+++ src/router.tsx
@@ -3,22 +3,29 @@ import { routeTree } from "./routeTree.gen";
 import { DefaultCatchBoundary } from "./components/DefaultCatchBoundary";
 import { NotFound } from "./components/NotFound";
 import { QueryClient } from "@tanstack/react-query";
-import { routerWithQueryClient } from "@tanstack/react-router-with-query";
+import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
 
 export function createRouter() {
   const queryClient = new QueryClient();
 
-  return routerWithQueryClient(
-    createTanStackRouter({
-      routeTree,
-      context: { queryClient, user: null },
-      defaultPreload: "intent",
-      defaultErrorComponent: DefaultCatchBoundary,
-      defaultNotFoundComponent: () => <NotFound />,
-      scrollRestoration: true,
-    }),
-    queryClient
-  );
+  const router = createTanStackRouter({
+    routeTree,
+    context: { queryClient, user: null },
+    defaultPreload: "intent",
+    defaultErrorComponent: DefaultCatchBoundary,
+    defaultNotFoundComponent: () => <NotFound />,
+    scrollRestoration: true,
+  });
+
+  setupRouterSsrQueryIntegration({
+    router,
+    queryClient,
+    // optional:
+    // handleRedirects: true,
+    // wrapQueryClient: true,
+  });
+
+  return router;
 }
 
 declare module "@tanstack/react-router" {
