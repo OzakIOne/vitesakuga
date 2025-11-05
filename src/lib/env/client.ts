feca93b89f66eb2bfbcc9b12505ba2c8ba611dc6
ozakione <29860391+OzakIOne@users.noreply.github.com>
2025-11-06 00:14:57 +0100

    update deps

--- src/lib/env/client.ts
+++ src/lib/env/client.ts
@@ -3,10 +3,10 @@ import { z } from "zod";
 const schema = z.object({
   BASE_URL: z.string(),
   VITE_BASE_URL: z.url(),
-  PROD: z.enum(["true", "false"]),
-  DEV: z.enum(["true", "false"]),
+  PROD: z.boolean(),
+  DEV: z.boolean(),
+  SSR: z.boolean(),
   MODE: z.enum(["development", "production", "test"]),
-  SSR: z.enum(["true", "false"]),
 });
 
 export const envClient = schema.parse(import.meta.env);
