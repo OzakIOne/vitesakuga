10d0d1fc70aafb3405c729e39993f0425765b836
ozakione <29860391+OzakIOne@users.noreply.github.com>
2026-01-04 20:50:56 +0100

    nitro config

---
+++ nitro.config.ts
@@ -0,0 +1,10 @@
+import { defineNitroConfig } from "nitro/config";
+
+export default defineNitroConfig({
+    compatibilityDate: "2025-01-01",
+    preset: "cloudflare_module",
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true
+    }
+})
